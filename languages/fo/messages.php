2648bc2a42fa224d75f6a336de7e394257d6cb97
Jógvan Olsen <jo@jebster.net>
2017-02-07 00:03:18 +0100

    Added faroese translations

---
+++ languages/fo/messages.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'Or donate via PayPal' => 'Ella stuðla ígjøgnum PayPal.',
+  'Amount' => '',
+  'Name' => 'Navn',
+  'One time donation' => 'Eingangsgávu',
+  'Create monthly payment' => 'Stovna fasta mánaðargávu',
+  'Donation can\'t be less than 1.' => 'Mást inntasta meira enn 1.',
+  'Repeating donation' => 'Fastur gevari',
+);
